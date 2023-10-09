544924c44516627d51ad3cde0a6a494676efb7af
Loïc Tallec <loictallec1@gmail.com>
2023-10-09 13:34:46 +0200

    Final Commit v2

--- assets/script.js
+++ assets/script.js
@@ -3,59 +3,43 @@
 // ******************** CONSTANTS ********************
 
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+  {
+    "image":"slide1.jpg",
+    "tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+  },
+  {
+    "image":"slide2.jpg",
+    "tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+  },
+  {
+    "image":"slide3.jpg",
+    "tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+  },
+  {
+    "image":"slide4.png",
+    "tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+  }
 ]
 
-
+const banner        = document.querySelector('#banner');
+const bannerImg     = document.querySelector('.banner-img');
+const bannerCaption = document.querySelector('#banner p');
+const arrowLeft     = document.querySelector('.arrow_left');
+const arrowRight    = document.querySelector('.arrow_right');
+const dotsContainer = document.querySelector('.dots');
+const dotsSelected  = document.querySelector('.dot_selected');
 
 // ******************** VARIABLES ********************
 
-const banner = document.getElementById('banner');
-
-const bannerImg = banner.querySelector('.banner-img');
-
-const bannerCaption = banner.querySelector('p');
-
-const arrowLeft = banner.querySelector('.arrow_left');
-
-const arrowRight = banner.querySelector('.arrow_right');
-
-const dotsContainer = document.querySelector('.dots');
-
-const dotsSelected = document.querySelector('.dot_selected');
-
 let currentIndex = 0;
-
-for (let i = 0; i < 4; i++) {
-	const dot = document.createElement('div');
-	dot.classList.add('dot');
-	dotsContainer.appendChild(dot);
-  
-	// Ajouter la classe "dot_selected" au premier dot
-	if (i === 0) {
-	  dot.classList.add('dot_selected');
-	}
-}
-
 
 // ******************** FUNCTIONS ********************
 
-// mise a jour de l'image du carrousel
+/**
+ * Updates the carousel with the slide at the specified index.
+ *
+ * @param {number} index - The index of the slide to be displayed.
+ */
 function updateCarousel(index) {
   const slide = slides[index];
   bannerImg.src = `./assets/images/slideshow/${slide.image}`;
@@ -64,7 +48,11 @@ function updateCarousel(index) {
   updateDots(index);
 }
 
-//mise a jour des dots 
+/**
+ * Update the dots in the dotsContainer based on the given index.
+ *
+ * @param {number} index - The index of the dot to be selected.
+ */
 function updateDots(index) {
   const dots = dotsContainer.querySelectorAll('.dot');
   dots.forEach((dotElement, dotIndex) => {
@@ -76,25 +64,62 @@ function updateDots(index) {
   });
 }
 
+/**
+ * Moves the carousel to the left by updating the current index and calling the updateCarousel function.
+ */
+function clickLeft() {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  updateCarousel(currentIndex);
+}
+
+/**
+ * Moves the carousel to the right by updating the current index and calling the updateCarousel function.
+ */
+function clickRight() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  updateCarousel(currentIndex);
+}
+
+/**
+ * Display the good image when clicking on a dot by calling the updateCarousel function. 
+ *
+ * @param {number} dotIndex - The index of the dot to click on.
+ */
+function clickDot(dotIndex) {
+  currentIndex = dotIndex;
+  updateCarousel(currentIndex);
+}
+
+/**
+ * Fills the dots container with four dot elements and adds the "dot_selected" class to the first dot.
+ */
+function fillDots() {
+  for (let i = 0; i < 4; i++) {
+    const dot = document.createElement('div');
+    dot.classList.add('dot');
+    dotsContainer.appendChild(dot);
+    
+    if (i === 0) {
+      dot.classList.add('dot_selected');
+    }
+  }
+}
+
+/**
+ * Adds event listeners to the arrow buttons and dots.
+ */
+function addlisteners() {
+  arrowLeft.addEventListener('click', clickLeft);
+  arrowRight.addEventListener('click', clickRight);
+  
+  dotsContainer.querySelectorAll('.dot').forEach((dot, index) => {
+    dot.addEventListener('click', function() {
+      clickDot(index);
+    });
+  });
+}
+
 // ******************** MAIN ********************
 
-// fleche de gauche
-arrowLeft.addEventListener('click', function() {
-	currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-	updateCarousel(currentIndex);
-  });
-  
-  // fleche de droite
-  arrowRight.addEventListener('click', function() {
-	currentIndex = (currentIndex + 1) % slides.length;
-	updateCarousel(currentIndex);
-  });
-
-  // Ajouter un écouteur d'événement à chaque dot
-dotsContainer.querySelectorAll('.dot').forEach((dot, index) => {
-	dot.addEventListener('click', function() {
-	  currentIndex = index;
-	  updateCarousel(currentIndex);
-	});
-  });
-  
+fillDots();
+addlisteners();
